6e5ae3319324f9de581cc938b4af0f2ea073dd02
mukul anand <MukulAnand@MUKULs-MacBook-Pro.local>
2024-06-24 01:59:48 +0530

    improve the code for folder structure

--- src/components/FolderStructure/FolderStructure.jsx
+++ src/components/FolderStructure/FolderStructure.jsx
@@ -1,5 +1,141 @@
 import React, { useState } from "react";
 
+const RenderBtn = ({ ele, index, initialSt, setStructure }) => {
+  const [name, setName] = useState("");
+  const [type, settype] = useState("folder");
+  const [display, setDisplay] = useState("none");
+  const onAddFolderFile = (ele, folder) => {
+    return folder.map((item) => {
+      if (item.name === ele.name && item.hasOwnProperty("content")) {
+        console.log({
+          ...item,
+          content: [...item.content, { name: name, type: type }],
+        });
+        return {
+          ...item,
+          content: [...item.content, { name: name, type: type }],
+        };
+      } else if (item.name === ele.name) {
+        return { ...item, content: [{ name: name, type: type }] };
+      } else if (item.hasOwnProperty("content")) {
+        return { ...item, content: [...onAddFolderFile(ele, item.content)] };
+      } else {
+        return { ...item };
+      }
+    });
+  };
+  return (
+    <div style={{ background: "pink", margin: "5px", cursor: "pointer" }}>
+      <div
+        onMouseEnter={() => setDisplay("flex")}
+        onMouseLeave={() => setDisplay("none")}
+      >
+        {ele.name}
+      </div>
+      <div
+        onMouseOver={() => setDisplay("flex")}
+        onMouseLeave={() => setDisplay("none")}
+        style={{ display: display }}
+        className="add-folder__wrapper"
+      >
+        <label for="folder-name">Folder Name</label>
+        <input
+          id="folder-name"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+        <select
+          onChange={(event) => {
+            settype(event.target.value);
+          }}
+          defaultValue="folder"
+        >
+          <option>folder</option>
+          <option>file</option>
+        </select>
+        <button
+          onClick={async () => {
+            const newARR = await onAddFolderFile(ele, initialSt);
+            setStructure(newARR);
+          }}
+        >
+          Create Folder Inside {ele.name}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const GetFolderSt = ({ st, id, initialSt, setStructure }) => {
+  const [show, updateShow] = useState({});
+  return st.map((ele, index) => {
+    return (
+      <div
+        key={`${id}${index}`}
+        style={{ paddingLeft: "10px", display: "flex" }}
+      >
+        {ele.type === "folder" && (
+          <button
+            key={`${id}${index}`}
+            style={{ maxHeight: "27px" }}
+            onClick={() =>
+              updateShow({
+                ...show,
+                [`${id}${index}`]: !show[`${id}${index}`],
+              })
+            }
+          >
+            V
+          </button>
+        )}
+        {ele.hasOwnProperty("content") ? (
+          <div style={{ width: "300px" }}>
+            {
+              <RenderBtn
+                initialSt={initialSt}
+                setStructure={setStructure}
+                ele={ele}
+                index={index}
+              />
+            }
+            <div style={{ paddingLeft: "25px" }}>
+              {show[`${id}${index}`] && (
+                <GetFolderSt
+                  key={`${id}${index}`}
+                  id={index}
+                  st={ele.content}
+                  initialSt={initialSt}
+                  setStructure={setStructure}
+                />
+              )}
+            </div>
+          </div>
+        ) : (
+          <div
+            style={{
+              background: ele.type === "folder" ? "pink" : "yellow",
+              margin: "10px",
+              width: "300px",
+            }}
+          >
+          
+            {ele.type === "folder" ? (
+              <RenderBtn
+                initialSt={initialSt}
+                setStructure={setStructure}
+                ele={ele}
+                index={index}
+              />
+            ) : (
+              ele.name
+            )}
+          </div>
+        )}
+      </div>
+    );
+  });
+};
+
 function FolderStructure() {
   const initialSructure = [
     {
@@ -57,105 +193,20 @@ function FolderStructure() {
   ];
 
   const [initialSt, setStructure] = useState(initialSructure);
-
-  const RenderBtn = ({ ele, index }) => {
-    const [name, setName] = useState("");
-    const [type, settype] = useState("folder");
-    const [display, setDisplay] = useState("none");
-    const onAddFolderFile = (ele, folder) => {
-      return folder.map((item) => {
-        if (item.name === ele.name && item.hasOwnProperty("content")) {
-          console.log({
-            ...item,
-            content: [...item.content, { name: name, type: type }],
-          });
-          return {
-            ...item,
-            content: [...item.content, { name: name, type: type }],
-          };
-        } else if (item.name === ele.name) {
-          return { ...item, content: [{ name: name, type: type }] };
-        } else if (item.hasOwnProperty("content")) {
-          return { ...item, content: [...onAddFolderFile(ele, item.content)] };
-        } else {
-          return { ...item };
-        }
-      });
-    };
-    return (
-      <div style={{ background: "pink", margin: "5px", cursor: "pointer" }}>
-        <div
-          onMouseEnter={() => setDisplay("flex")}
-          onMouseLeave={() => setDisplay("none")}
-        >
-          {ele.name}
-        </div>
-        <div
-          onMouseOver={() => setDisplay("flex")}
-          onMouseLeave={() => setDisplay("none")}
-          style={{ display: display }}
-          className="add-folder__wrapper"
-        >
-          <label for="folder-name">Folder Name</label>
-          <input
-            id="folder-name"
-            value={name}
-            onChange={(event) => setName(event.target.value)}
-          />
-          <select
-            onChange={(event) => {
-              settype(event.target.value);
-            }}
-            defaultValue="folder"
-          >
-            <option>folder</option>
-            <option>file</option>
-          </select>
-          <button
-            onClick={async () => {
-              const newARR = await onAddFolderFile(ele, initialSt);
-              console.log(newARR, "newArr");
-              setStructure(newARR);
-            }}
-          >
-            Create Folder Inside {ele.name}
-          </button>
-        </div>
-      </div>
-    );
-  };
-
-  const getFolderSt = (st) =>
-    st.map((ele, index) => (
-      <div style={{ paddingLeft: "10px" }}>
-        {ele.hasOwnProperty("content") ? (
-          <div style={{ width: "300px" }}>
-            {<RenderBtn ele={ele} index={index} />}
-            <div style={{ paddingLeft: "25px" }}>
-              {getFolderSt(ele.content)}
-            </div>
-          </div>
-        ) : (
-          <div
-            style={{
-              background: ele.type === "folder" ? "pink" : "yellow",
-              margin: "10px",
-              width: "300px",
-            }}
-          >
-            {ele.type === "folder" ? (
-              <RenderBtn ele={ele} index={index} />
-            ) : (
-              ele.name
-            )}
-          </div>
-        )}
-      </div>
-    ));
-  return <div className="folder-structure">
-    <h1>Folder Structure</h1>
-    {getFolderSt(initialSt)}
-    </div>;
+  return (
+    <div className="folder-structure">
+      <h1>Folder Structure</h1>
+      <h6>hover over the folder below to create new</h6>
+      {
+        <GetFolderSt
+          st={initialSt}
+          initialSt={initialSt}
+          setStructure={setStructure}
+          id=""
+        />
+      }
+    </div>
+  );
 }
 
 export default FolderStructure;
